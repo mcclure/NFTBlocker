448e46307031afc9463dc961bf3facf700388f70
Cecelia Wren <cecelia.wren@gmail.com>
2015-03-19 21:45:17 -0400

    Added jQuery, jquery.datatables, and data table for blocking receipts

--- options.js
+++ options.js
@@ -1,3 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
     // populate table here
+    chrome.storage.local.get("blockingReceipts",function(items) {
+        var receipts = items.blockingReceipts;
+        var dataset = [];
+        for(var username in receipts) {
+            var receipt = receipts[username];
+            dataset.push( [ username, receipt.following, new Date(receipt.on).toString() ] );
+        }
+        $('#receiptsTable').dataTable({
+            "data": dataset,
+            "columns": [
+                { "title": "Blocked User" },
+                { "title": "Following/Follower" },
+                { "title": "Date Blocked" }
+            ]
+        }); 
+    });
 });
