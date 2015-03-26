853f0dbeb735fa721d61d82ea07d02a70acc2745
Cecelia Wren <cecelia.wren@gmail.com>
2015-03-25 20:02:01 -0400

    added partials for blocking receipts, whitelist, and user add form. added datatables bower dependency.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,8 +19,13 @@ module.exports = function(grunt) {
                     {src: 'CONTRIBUTORS', dest: ''},
                     {src: 'README.md', dest: ''},
                     {src: 'bower_components/angular/angular.min.js', dest: 'bower_components/angular/'},
+                    {src: 'bower_components/angular-route/angular-route.min.js', dest: 'bower_components/angular-route/'},
                     {src: 'bower_components/bootstrap/dist/css/bootstrap.min.js', dest: 'bower_components/bootstrap/dist/css/'},
                     {src: 'bower_components/jquery/dist/jquery.min.js', dest: 'bower_components/jquery/dist/'},
+                    {src: 'bower_components/angular-datatables/dist/angular-datatables.min.js', dest: 'bower_components/angular-datatables/dist/'},
+                    {src: 'bower_components/datatables/media/js/jquery.dataTables.min.js', dest: 'bower_components/datatables/media/js/'},
+                    {src: 'bower_components/datatables/media/css/jquery.dataTables.min.css', dest: 'bower_components/datatables/media/css/'},
+                    {src: 'bower_components/datatables/media/images/*', dest: 'bower_components/datatables/media/images/'},
                 ]
             }
         }
--- js/options.js
+++ js/options.js
@@ -1,19 +1,122 @@
 $(document).ready(function() {
     // populate table here
-    chrome.storage.local.get("blockingReceipts",function(items) {
-        var receipts = items.blockingReceipts;
-        var dataset = [];
-        for(var username in receipts) {
-            var receipt = receipts[username];
-            dataset.push( [ username, receipt.type + ": " + receipt.connection, new Date(receipt.on).toString() ] );
-        }
-        $('#receiptsTable').dataTable({
-            "data": dataset,
-            "columns": [
-                { "title": "Blocked User" },
-                { "title": "Connection To" },
-                { "title": "Date Blocked" }
-            ]
-        }); 
+    
+});
+var optionsApp = angular.module('twBlockchain', ['ngRoute','kcd.directives','datatables'])
+optionsApp.config(['$routeProvider',
+function($routeProvider) {
+    $routeProvider.
+    when('/receipts', {
+        templateUrl: 'partial_receipts.html',
+        controller: 'ReceiptsController'
+    }).
+    when('/protected', {
+        templateUrl: 'partial_protects.html',
+        controller: 'ProtectedController'
+    }).
+    when('/add-protection', {
+        templateUrl: 'partial_add_protection.html',
+        controller: 'AddProtectionController'
+    }).
+    otherwise({
+        redirectTo: '/receipts'
     });
+}]);
+optionsApp.controller('ReceiptsController', ['$scope', function($scope) {
+    $scope.should = {};
+    $scope.should.refreshTable = false;
+    chrome.storage.local.get("blockingReceipts",function(items) {
+        var dataset = [];
+        for(var username in items.blockingReceipts) {
+            var receipt = items.blockingReceipts[username];
+            receipt.username = username;
+            dataset.push( receipt );
+        }
+        $scope.receipts = dataset;
+        $scope.should.refreshTable = true;
+    });
+}]);
+
+optionsApp.controller('ProtectedController', ['$scope', function($scope) {
+    $scope.should = {};
+    $scope.should.refreshTable = false;
+    
+    chrome.storage.sync.get("whitelist",function(items) {
+        var dataset=[];
+        for(var username in items.whitelist) {
+            var receipt = items.whitelist[username];
+            dataset.push( receipt );
+        }
+        $scope.protectedUsers = dataset;
+        $scope.should.refreshTable = true;
+    });
+}]);
+optionsApp.controller('AddProtectionController', ['$scope', '$location', function($scope, $location) {
+    $scope.user = {username: "", on: Date.now()};
+    $scope.error = false;
+    $scope.submit = function() {
+        $scope.error = false;
+        if ($scope.user.username == "" || !$scope.user.username) {
+            $scope.error = "You must enter a username";
+            return;
+        }
+        chrome.storage.sync.get("whitelist",function(items) {
+            if (typeof items.whitelist === "undefined") {
+                items.whitelist = {};
+            }
+            items.whitelist[$scope.user.username] = $scope.user;
+            chrome.storage.sync.set({ whitelist: items.whitelist }, function() {
+                $location.url('/protected');
+                $scope.$apply();
+            });
+        });
+    };
+}]);
+optionsApp.controller('NavigationController', ['$scope', '$location', function($scope, $location) {
+    $scope.isActive = function(path) {
+        return ($location.url().indexOf(path) > -1);
+    }
+}]);
+    
+    
+angular.module('kcd.directives',[]).directive('kcdRecompile', function($compile, $parse) {
+  'use strict';
+
+  return {
+    scope: true, // required to be able to clear watchers safely
+    compile: function(el) {
+      var template = el.html();
+      return function link(scope, $el, attrs) {
+        scope.$parent.$watch(attrs.kcdRecompile, function(_new, _old) {
+          var useBoolean = attrs.hasOwnProperty('useBoolean');
+          if ((useBoolean && (!_new || _new === 'false')) || (!useBoolean && (!_new || _new === _old))) {
+            return;
+          }
+          // remove all watchers because the recompiled version will set them up again.
+          removeChildrenWatchers($el);
+          // reset kcdRecompile to false if we're using a boolean
+          if (useBoolean) {
+            $parse(attrs.kcdRecompile).assign(scope.$parent, false);
+          }
+
+          // recompile
+          var newEl = $compile(template)(scope.$parent.$new());
+          $el.html(newEl);
+        });
+      };
+    }
+  };
+
+  function removeChildrenWatchers(element) {
+    angular.forEach(element.children(), function(childElement) {
+      removeAllWatchers(angular.element(childElement));
+    });
+  }
+
+  function removeAllWatchers(element) {
+    if (element.data().hasOwnProperty('$scope')) {
+      element.data().$scope.$$watchers = [];
+    }
+    removeChildrenWatchers(element);
+  }
 });
