e2560fbc91994d9c7ec55bdde1b6f21f1d97c0db
Cecelia Wren <cecelia.wren@gmail.com>
2015-03-29 14:46:14 -0400

    working towards adding an options page.

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,38 +1,62 @@
 var buttons = require('sdk/ui/button/action');
+var { ToggleButton } = require('sdk/ui/button/toggle');
 var tabs = require("sdk/tabs");
 var data = require("sdk/self").data;
 var pageMod = require("sdk/page-mod");
 var array = require('sdk/util/array');
 var workers = [];
+var self = require("sdk/self");
 
 var popup = require("sdk/panel").Panel({
-  contentURL: data.url("popup.html"),
-  contentScriptFile: data.url("popup.js")
+    contentURL: data.url("popup.html"),
+    contentScriptFile: data.url("popup.js")
 });
+
 popup.on('click', function() {
     popup.hide();
 });
 
-var button = buttons.ActionButton({
-  id: "twitter-block-chain-link",
-  label: "Twitter Block Chain",
-  icon: {
+var contextPanel = require("sdk/panel").Panel({
+    contentURL: data.url("contextmenu.html"),
+    onHide: handleHide
+});
+
+function handleHide() {
+    button.state('window', {checked: false});
+}
+
+contextPanel.on("click",function(option) {
+    if (option == "blockAll") {
+        var worker = findWorkerForCurrentTab();
+        if (worker) {
+            worker.port.emit("blockChainStart", true);
+        }
+        else {
+            popup.show();
+        }
+    }
+    else {
+        require("sdk/tabs").open(data.url("options.html"));
+    }    
+});
+
+var button = ToggleButton({
+    id: "twitter-block-chain-link",
+    label: "Twitter Block Chain",
+    icon: {
     "16": "./icon16.png",
     "32": "./icon32.png",
     "64": "./icon64.png"
-  },
-  onClick: handleClick
+    },
+    onChange: handleChange
 });
 
-function handleClick(state) {
-    var worker = findWorkerForCurrentTab();
-    if (worker) {
-        worker.port.emit("blockChainStart", true);
-    }
-    else {
-        popup.show();
-    }
-    //woerk.port.emit("blockChainStart", true);
+function handleChange(state) {
+  if (state.checked) {
+    contextPanel.show({
+      position: button
+    });
+  }
 }
 
 function findWorkerForCurrentTab(){
