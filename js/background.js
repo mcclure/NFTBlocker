65a84438eadf9181688bddb2a51018dd2149e584
Cecelia Wren <cecelia.wren@gmail.com>
2019-05-19 18:47:08 -0400

    removes dead code

--- js/background.js
+++ js/background.js
@@ -43,25 +43,6 @@ function _makeRequest(obj) {
             throw new Error(response.statusText);
         }
     })
-    return new Promise((resolve, reject) => {
-        let xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        xhr.open(obj.method || "GET", obj.url);
-        if (obj.headers) {
-            Object.keys(obj.headers).forEach(key => {
-                xhr.setRequestHeader(key, obj.headers[key]);
-            });
-        }
-        xhr.onload = () => {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.response);
-            } else {
-                reject(xhr.statusText);
-            }
-        };
-        xhr.onerror = () => reject(xhr.statusText);
-        xhr.send(obj.body);
-    });
 }
 // request.user_id
 // request.CSRFCookie
